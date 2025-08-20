5ddbde1639d182e8d24077d0eb7c325ef3b01957
VictorLukumbu <vlukumbu@gmail.com>
2025-08-20 15:28:00 +0200

    Delete script.js
    remove a duplicate script.js file from the root

--- script.js
+++
@@ -1,6 +0,0 @@
-// Here document.addEventListener will wait until the page is fully loaded before 
-// running code.
-// And console.log will log a message to the browser console (for testing/debugging) 
-document.addEventListener("DOMContentLoaded", () => {
-    console.log("Victor’s site is live and kicking!");
-  });
